a55f744f28f9bb08cf2d7d7174a96b7d7f6d0514
kjs001127 <camel@Camelui-MacBookPro.local>
2023-04-24 13:55:21 +0900

    fix: fix compile errors and panic bugs

--- pkg/jitter/listener.go
+++ pkg/jitter/listener.go
@@ -11,21 +11,13 @@ type NullListener struct {
 }
 
 func (n NullListener) OnPacketLoss(currentTs, remainingTs int64) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (n NullListener) OnLatencyChanged(new int64) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (n NullListener) OnPacketEnqueue(currentTs, remainingTs int64, pkt *Packet) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (n NullListener) OnPacketDequeue(currentTs, remainingTs int64, pkt []*Packet) {
-	//TODO implement me
-	panic("implement me")
 }
