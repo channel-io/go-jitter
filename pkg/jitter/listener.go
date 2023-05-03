70c26286879a0814919583efc141901a24b9b781
kjs001127 <camel@Camelui-MacBookPro.local>
2023-05-03 11:47:33 +0900

    feat: fix to compare with currentTime when determining re-sync

--- pkg/jitter/listener.go
+++ pkg/jitter/listener.go
@@ -1,10 +1,11 @@
 package jitter
 
 type Listener interface {
-	OnPacketLoss(currentTs, remainingTs int64)
-	OnPacketEnqueue(currentTs, remainingTs int64, pkt *Packet)
-	OnPacketDequeue(currentTs, remainingTs int64, pkt []*Packet)
-	OnLatencyChanged(old, new int64)
+	OnPacketLoss(targetTs, remainingTs int64)
+	OnPacketEnqueue(targetTs, remainingTs int64, pkt *Packet)
+	OnPacketDequeue(targetTs, remainingTs int64, pkt []*Packet)
+	OnLatencyChanged(new int64)
+	OnReSyncTriggered(oldCurrent int64, newCurrent int64)
 }
 
 type NullListener struct {
@@ -13,11 +14,15 @@ type NullListener struct {
 func (n NullListener) OnPacketLoss(currentTs, remainingTs int64) {
 }
 
-func (n NullListener) OnLatencyChanged(old, new int64) {
+func (n NullListener) OnLatencyChanged(new int64) {
 }
 
 func (n NullListener) OnPacketEnqueue(currentTs, remainingTs int64, pkt *Packet) {
 }
 
 func (n NullListener) OnPacketDequeue(currentTs, remainingTs int64, pkt []*Packet) {
+}
+
+func (n NullListener) OnReSyncTriggered(oldCurrent int64, newCurrent int64) {
+
 }
